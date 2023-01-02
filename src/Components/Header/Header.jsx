7b8b16a0c6105ab692c3305441d8d1d5af51ae5b
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-01-02 01:33:31 -0300

    Navbar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useWindowSize } from "../../Hooks/useWindowSize";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import LinkButtons from "./LinkButtons";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const windowSize = useWindowSize();
+
+  return (
+    <>
+      <div className="h-16 w-full text-white font-bold text-2xl">
+        {windowSize.width > 1024 && (
+          <div className="flex justify-end items-center gap-10 px-10 py-5 transition-all">
+            <LinkButtons />
+          </div>
+        )}
+
+        {windowSize.width <= 1024 && (
+          <div className="transition-all absolute right-0 w-full sm:w-72">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="absolute right-5 top-5 z-10"
+            >
+              {!isOpen ? (
+                <AiOutlineMenu size={40} />
+              ) : (
+                <AiOutlineClose size={40} />
+              )}
+            </button>
+            <div
+              className={`relative flex flex-col justify-end items-center gap-10 pt-16 pb-7 rounded-b-xl ${
+                isOpen ? "opacity-100 bg-backgroundLight" : "opacity-0"
+              } transition-all`}
+            >
+              <LinkButtons />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      gray: "#CCCCCC",
+      gold: "#DCAC58",
+      backgroundLight: "#252836",
+      backgroundDark: "#1F1D2B",
+    },
   },
   plugins: [],
 };
