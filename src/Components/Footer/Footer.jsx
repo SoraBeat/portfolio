ba9ef1c8f3029bc41bf8275c05380489e8084604
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-01-02 19:36:08 -0300

    Home y animaciones

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center'>
+        <p className='text-white text-xl z-10 absolute bottom-2'>© 2023 Lautaro Elian Roa Mazzola reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
