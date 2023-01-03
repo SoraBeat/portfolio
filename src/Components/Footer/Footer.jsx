a3308154a5ee6f1efb8623f0a5d9dc1460a73197
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-01-03 15:42:28 -0300

    Animaciones entre rutas y fix bugs

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-center'>
-        <p className='text-white text-xl z-10 absolute bottom-2'>© 2023 Lautaro Elian Roa Mazzola reserved.</p>
+        <p className='text-white text-xl z-10  lg:absolute bottom-2'>© 2023 Lautaro Elian Roa Mazzola reserved.</p>
     </div>
   )
 }
