a068760bdc3b6373389e3a9826c9f06e6862b6ee
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-01-04 17:26:15 -0300

    Animacion loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import AnimatedDiv from "../AnimatedDiv/AnimatedDiv";
 import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="w-screen flex-col h-screen flex justify-center items-center gap-5">
-      <h1 className="text-white text-4xl font-bold">Cargando</h1>
-      <span className="loader"></span>
-    </div>
+    <AnimatedDiv>
+      <div className="w-screen flex-col h-screen flex justify-center items-center gap-5">
+        <h1 className="text-white text-4xl font-bold">Cargando</h1>
+        <span className="loader"></span>
+      </div>
+    </AnimatedDiv>
   );
 };
 
